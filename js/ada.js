c2a299c9267086a98ddb1427ff7e1ec0f1af734a
Cory Monteleone <cmontele@rcgc.edu>
2018-09-04 09:59:07 -0400

    Menu has keyboard controls now. This completes a feature request. Awaiting testing.

--- js/ada.js
+++ js/ada.js
@@ -3,4 +3,28 @@ jQuery(document).ready( function() {
     tabindex: '-1',
     id: 'main',
   });
+
+  function fixSharePointSearchBox() {
+    var searchbox = $("#SearchBox input");
+    searchbox.removeAttr("accesskey"); // remove access-key
+    searchbox.attr("aria-label", "Search").attr("name", "search");
+    searchbox.before("<label for='" + searchbox.attr("id") + "' class='sr-only'>Search: </label>");
+  }
+
+  function fixMegaMenu() {
+    $("nav:first").accessibleMegaMenu({
+        uuidPrefix: "menu-link",
+        menuClass: "cd-primary-menu",
+        topNavItemClass: "top-nav-item",
+        panelClass: "sub-nav",
+        panelGroupClass: "sub-nav-group",
+        hoverClass: "hover",
+        focusClass: "focus",
+        openClass: "open",
+        debug: "true"
+    });
+  }
+  
+  fixSharePointSearchBox();
+  fixMegaMenu();
 });
