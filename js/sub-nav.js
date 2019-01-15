8c97c20764974246a53679706b1798ef2396c75d
Cory Monteleone <cmontele@rcgc.edu>
2019-01-15 12:39:15 -0500

    Added in the basics of the subsite menu. Revisions and refactors to come soon

---
+++ js/sub-nav.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {
+  $("#subsite-navigation").children('.subsite-navigation__top').children('a').on('click', function(event){
+      var selected = $(this);
+      if( selected.next('.subsite-navigation__drop').hasClass('d-none') ) {
+        selected.addClass('selected').next('.subsite-navigation__drop').removeClass('d-none').attr('aria-expanded', 'true');
+      } else {
+        selected.removeClass('selected').next('.subsite-navigation__drop').addClass('d-none').attr('aria-expanded', 'false');
+      }
+  });
+}); 
