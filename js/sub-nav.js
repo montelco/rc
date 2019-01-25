8981d83655e4d9468d7d07853af40c0779dd4968
Cory Monteleone <cmontele@rcgc.edu>
2019-01-25 09:20:28 -0500

    Updates for the secondary layout

--- js/sub-nav.js
+++ js/sub-nav.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function() {
   $("#subsite-navigation").children('.subsite-navigation__top').children('a').on('click', function(event){
+      event.preventDefault();
       var selected = $(this);
       if( selected.next('.subsite-navigation__drop').hasClass('d-none') ) {
         selected.addClass('selected').next('.subsite-navigation__drop').removeClass('d-none').attr('aria-expanded', 'true');
