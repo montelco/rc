1449eb0fe99425259d6df6c3f2144cb0e9a76375
Cory Monteleone <cmontele@rcgc.edu>
2019-03-13 07:41:45 -0400

    Working on getting the subsite navigation to properly collapse on mobile devices.

--- js/sub-nav.js
+++ js/sub-nav.js
@@ -1,11 +1,47 @@
 jQuery(document).ready(function() {
+  var mobile = 576;
+  checkWindowWidth();
+
+  $(window).on('resize', function() {
+      checkWindowWidth();
+  });
+
   $("#subsite-navigation").children('.subsite-navigation__top').children('a').on('click', function(event){
       event.preventDefault();
       var selected = $(this);
       if( selected.next('.subsite-navigation__drop').hasClass('d-none') ) {
-        selected.addClass('selected').next('.subsite-navigation__drop').removeClass('d-none').attr('aria-expanded', 'true');
+        selected.addClass('selected').next('.subsite-navigation__drop').addClass('position-xs-static').removeClass('d-none').attr('aria-expanded', 'true');
       } else {
-        selected.removeClass('selected').next('.subsite-navigation__drop').addClass('d-none').attr('aria-expanded', 'false');
+        selected.removeClass('selected').next('.subsite-navigation__drop').removeClass('position-xs-static').addClass('d-none').attr('aria-expanded', 'false');
       }
   });
+
+  if (!checkWindowWidth()) {
+    var navContainer = $(".subsite");
+    var nav = $("#subsite-navigation");
+    var site = $("header.hero h1").text();
+    $("#subsite-navigation").children('li.top-nav-item').children('a').addClass('d-block');
+    $("#site-name").append("<a href='#'>" + site + " Menu</a>");
+    nav.addClass('d-none');
+
+    $("#site-name").children('a').on('click', function(event){
+      event.preventDefault();
+      $("#subsite-navigation").toggleClass('d-none');
+      $("#subsite-navigation").toggleClass('d-block');
+    });
+  }
+
+  function checkWindowWidth() {
+    var e = window
+      , a = 'inner';
+    if (!('innerWidth'in window)) {
+        a = 'client';
+        e = document.documentElement || document.body;
+    }
+    if (e[a + 'Width'] >= mobile) {
+        return true;
+    } else {
+        return false;
+    }
+  }
 }); 
