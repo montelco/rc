798d116c73638f6a4cf7e4b4ca6cbb77a86c280b
Cory Monteleone <cmontele@rcgc.edu>
2019-09-19 07:58:35 -0400

    Adding the webpack files. They were split into environments for simplicity.

---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+const path = require("path");
+module.exports {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  mode: "production",
+  entry: "./src/main.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
