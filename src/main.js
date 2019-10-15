01883e88c4ab49af548365f4ecf1b6ab02664e4b
Monteleone-Haught <cmontele@rcsj.edu>
2019-10-15 14:49:12 -0400

    Extracted a utility function and added cookie logic for autopausing video

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import {getCookieValue} from './getCookieValue.js';
+import {setCookieValue} from './setCookieValue.js';
 import './ada.js';
 import './preferred.js';
 import './video.js';
--- src/video.js
+++ src/video.js
@@ -2,17 +2,23 @@ import {getCookieValue} from './getCookieValue.js';
 
 const video = document.getElementById('bgvid');
 const vTrimControl = document.getElementById('vTrimControl');
+let present = isVideoThere(video);
+let playState = getCookieValue('video-state');
 
 window.onload = () => {
-  let present = isVideoThere(video);
-  if (present) {rememberPreference()}
-};
+  if (present && playState == "paused") {
+    video.pause();
+    vTrimControl.classList.toggle('pause');
+    return true;
+  }
+}
 
 vTrimControl.addEventListener("click", function() {
   if (video.paused == true) {
     video.play();
   } else {
     video.pause();
+    setCookieValue("video-state", "paused", 7);
   }
   vTrimControl.classList.toggle('pause');
   return true;
@@ -25,9 +31,4 @@ export function isVideoThere(video) {
     return false;
   }
   return false;
-}
-
-export function rememberPreference() {
-  let pausedPreference = getCookieValue('video-state');
-  return console.log(pausedPreference);
 }
